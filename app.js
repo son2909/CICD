1482c48227d57dac4f77c8c275cf792cbdccace1
root <root@ip-172-31-35-106.ap-southeast-1.compute.internal>
2020-09-09 08:02:33 +0000

    make

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
-const express = require('express');
-const app = express();
-
-
-app.get('/', (req, res) => {
-  res.send('Hello docker !');
-})
-
-app.listen(3000, ()=> (console.log('Server start on port 3000 !')));
+//importing node framework
+var express = require('express');
+ 
+var app = express();
+//Respond with "hello world" for requests that hit our root "/"
+app.get('/', function (req, res) {
+ res.send('hello world');
+});
+//listen to port 3000 by default
+app.listen(3000, ()=> {
+	console.log('server running on port 3000')
+});
+ 
+module.exports = app;
